763440bca5b1e109ac1c0f9997740ccd8e4d659e
Ted Sanders <tedsanders@stanford.edu>
2013-12-20 17:30:43 -0800

    trying again

--- science.php
+++
@@ -1,8 +0,0 @@
-      <!DOCTYPE html>
-      <html>
-      <head>
-      <link rel="canonical" href="/science"/>
-      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-      <meta http-equiv="refresh" content="0;url=/science" />
-      </head>
-      </html>
--- tax-tips-for-phd-students.php
+++
@@ -1,8 +0,0 @@
-      <!DOCTYPE html>
-      <html>
-      <head>
-      <link rel="canonical" href="/tax-tips-for-phd-students"/>
-      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-      <meta http-equiv="refresh" content="0;url=/tax-tips-for-phd-students" />
-      </head>
-      </html>
