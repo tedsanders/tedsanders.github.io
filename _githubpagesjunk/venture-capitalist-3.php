db0c1189eecdf524661da19bf7fbcca19c4f48cb
Ted Sanders <tedsanders@stanford.edu>
2014-01-08 21:42:43 -0800

    uhhhh

--- _githubpagesjunk/venture-capitalist-3.php
+++
@@ -1,8 +0,0 @@
-      <!DOCTYPE html>
-      <html>
-      <head>
-      <link rel="canonical" href="/venture-capitalist-3"/>
-      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-      <meta http-equiv="refresh" content="0;url=/venture-capitalist-3" />
-      </head>
-      </html>
