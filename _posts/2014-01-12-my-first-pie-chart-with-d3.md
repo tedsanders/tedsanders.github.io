681bd8bc1bda4bacc677e3494fef43018dc5e87a
Ted Sanders <tedsanders@stanford.edu>
2014-01-12 18:38:02 -0800

    horoscope blog post!

--- _posts/2014-01-12-my-first-pie-chart-with-d3.md
+++ _posts/2014-01-12-my-first-pie-chart-with-d3.md
@@ -1,7 +1,7 @@
 ---
 layout: default
-description: Ted graphs his first graph.
-title: My first pie chart
+description: My first d3 pie chart.
+title: My first pie chart with d3
 subtitle:
 categories: graphs
 tags:
@@ -10,12 +10,14 @@ hide:
 comments:
 ---
 
+<div id="viz"></div>
+
 Legend:
 
 White - the success I had in making a pie chart
 
 Black - the success I had in making a pie chart with multiple slices
 
-<div id="viz"></div>
+
 
 <script src="{{site.url}}/js/my-first-pie-chart.js"></script>
--- js/my-first-pie-chart.js
+++ js/my-first-pie-chart.js
@@ -1,13 +1,11 @@
     var sampleSVG = d3.select("#viz")
         .append("svg")
-        .attr("width", 100)
-        .attr("height", 100);    
+        .attr("width", 500)
+        .attr("height", 500);    
 
     sampleSVG.append("circle")
         .style("stroke", "gray")
         .style("fill", "white")
-        .attr("r", 40)
-        .attr("cx", 50)
-        .attr("cy", 50)
-        .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
-        .on("mouseout", function(){d3.select(this).style("fill", "white");});
+        .attr("r", 240)
+        .attr("cx", 250)
+        .attr("cy", 250)
